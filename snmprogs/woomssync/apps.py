c0be4b41c4f65b0824172ece26e99045d81fe4c0
nsr <serheos@bk.ru>
2024-12-19 00:28:44 +0300

    remove woomssync

--- snmprogs/woomssync/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from root.apps import SnmAppBase
-
-
-class WooMsSyncConfig(AppConfig, SnmAppBase):
-    name = 'woomssync'
-    verbose_name = "Синхронизация Woocommerce"
-    display_in_menu = False
