3fdd084cc8157d889041e16b65f20d46b31c878b
nsr <serheos@bk.ru>
2024-12-19 01:03:49 +0300

    fix ruff D

--- snmprogs/snmprogs/debug_settings.py
+++ snmprogs/snmprogs/debug_settings.py
@@ -1,5 +1,5 @@
 """
-Дебажная конфигурация сайта. Для запуска скопировать с именем settings.py
+Дебажная конфигурация сайта. Для запуска скопировать с именем settings.py.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/topics/settings/
