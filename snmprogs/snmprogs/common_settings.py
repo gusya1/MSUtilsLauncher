b1e7d7459853116389f31a60774fafef27781d07
nsr <serheos@bk.ru>
2024-12-19 00:20:45 +0300

    fix settings files

--- snmprogs/snmprogs/common_settings.py
+++ snmprogs/snmprogs/common_settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-from MSApi import MSApi
-
-MSApi.set_access_token('4f31f17f521271b666e44f13ffe50fbf63d575a5')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -98,8 +95,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = 'media/'
@@ -108,8 +103,6 @@ TEMP_URL = 'tmp/'
 TEMP_ROOT = os.path.join(BASE_DIR, 'tmp/')
 
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
